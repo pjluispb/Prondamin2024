2671abb7131bc3d8e3d31ac35f313da0fdad13b6
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-11 15:43:04 -0400

    Update uplocvs.py

--- pages/uplocvs.py
+++ pages/uplocvs.py
@@ -12,7 +12,27 @@ uploaded_file = st.file_uploader("Cargar archivo CSV", type=["csv"])
 
 if uploaded_file is not None:
     # Lee el archivo CSV en un DataFrame
+    # Leer el archivo CSV
     df = pd.read_csv(uploaded_file)
+
+    # Verificar si el archivo es CSV
+    if not uploaded_file.name.lower().endswith('.csv'):
+        st.error('El archivo seleccionado no es un archivo CSV válido.')
+        return
+
+    # Verificar si la cabecera cumple con los campos requeridos
+    if not check_csv_header(df.columns):
+        st.error('El archivo CSV debe tener las siguientes columnas: Fecha, Descripcion, Referencia, Egreso, Ingreso.')
+        return
+
+    # Mostrar el DataFrame si todas las verificaciones son exitosas
+    st.header('Contenido del archivo CSV')
+    st.write(df)
+
+
+
+    
+    #df = pd.read_csv(uploaded_file)
     dfingresoXpm = df[df['DESCRIPCION'] == 'NC - PAGO MOVIL INTERBANCARIO']
     dfingresoXtrans = df[df['DESCRIPCION'] == 'NC - TRANSFERENCIA DE FONDOS VIA INTERNET']
     frames = [dfingresoXpm, dfingresoXtrans]
