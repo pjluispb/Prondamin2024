c2d9c39696acf1e97ce3303f7ad22cb9080fc769
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-11 16:03:21 -0400

    Update uplocvs.py

--- pages/uplocvs.py
+++ pages/uplocvs.py
@@ -3,6 +3,53 @@ import streamlit as st
 import pandas as pd
 from deta import Deta
 
+deta = Deta(st.secrets["deta_key"])
+
+def row_style(row):
+    if row['paycon'] == 'SI++':
+        return pd.Series('background-color: #7986cb; color:#000000', row.index)
+    elif row['paycon'] == 'PENDIENTE X DIFERENCIA':
+        return pd.Series('background-color: #ff5079; color:#000000', row.index)
+    elif row['paycon'] == 'SI':
+        return pd.Series('background-color: #8ede99; color:#000000', row.index)
+    elif row['paycon'] == 'PENDIENTE':
+        return pd.Series('background-color: #fdd834; color:#000000', row.index)
+    else:
+        return pd.Series('', row.index)
+
+def update_paycon(row):
+    fmontoPago = float(row['montoPago']) if row['montoPago'] not in ('-', None, '') else 0
+    fmontoApagar = float(row['montoApagar']) if row['montoApagar'] not in ('-', None, '') else 0
+    if row['paycon'] not in ('PENDIENTE', 'NO'):
+        diferencia = fmontoPago - fmontoApagar
+        print(diferencia)
+        if diferencia!=0:
+            if -10 <= diferencia <= 10:
+                return 'SI'
+            elif diferencia < -10:
+                return 'PENDIENTE X DIFERENCIA'
+            elif diferencia > 10:
+                return 'SI++'
+            else:
+                return row['paycon']  # Keep the original value if none of the conditions apply
+    else:
+        return row['paycon']
+
+def color_paycon(val):
+    color = 'green' if val == 'SI' else 'white'
+    return 'background-color: %s' % color   
+
+# Carga el Pronda
+Prondamin24 = deta.Base('Pronda24Test')
+Pronda24 = Prondamin24.fetch()
+dfPronda24 = pd.DataFrame(Pronda24.items)
+# dfPronda24
+
+# Carga el DBanVerif2024 ...Datos Bancarios ya procesados
+DBanV24 = deta.Base('DBanVerif2024')
+DBanV24f = DBanV24.fetch().items
+dfDBanV24 = pd.DataFrame(DBanV24f)
+
 def check_csv_header(header):
     #required_columns = ['Fecha', 'Descripcion', 'Referencia', 'Egreso', 'Ingreso']
     required_columns = ['FECHA', 'DESCRIPCION', 'REFERENCIA', 'EGRESO', 'INGRESO']
