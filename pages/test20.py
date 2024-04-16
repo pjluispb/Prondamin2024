486c44e4e9f339fc40c4d9668a4f2039be0c347c
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-16 15:48:05 -0400

    Update test20.py

--- pages/test20.py
+++ pages/test20.py
@@ -11,6 +11,19 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
+def row_style(row):
+    if row['paycon'] == 'SI++':
+        return pd.Series('background-color: #7986cb; color:#000000', row.index)
+    elif row['paycon'] == 'PENDIENTE X DIFERENCIA':
+        return pd.Series('background-color: #ff5079; color:#000000', row.index)
+    elif row['paycon'] == 'SI':
+        return pd.Series('background-color: #8ede99; color:#000000', row.index)
+    elif row['paycon'] == 'PENDIENTE':
+        return pd.Series('background-color: #fdd834; color:#000000', row.index)
+    else:
+        return pd.Series('', row.index)
+
+
 pronda2024 = deta.Base('Prondamin2024C')
 p24 = pronda2024.fetch(limit=5000)
 dfp24 = pd.DataFrame(p24.items)
