285e7743790d0077b973324c04617818634218fb
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-17 16:33:33 -0400

    Create cambiakeys.py

---
+++ pages/cambiakeys.py
@@ -0,0 +1,28 @@
+
+import streamlit as st
+import pandas as pd
+from deta import Deta
+from PIL import Image
+
+
+
+deta = Deta(st.secrets["deta_key"])
+
+uploaded_file = st.file_uploader("Subir archivo CSV", type=["csv"]) 
+dfu = pd.read_csv(uploaded_file)
+
+# Carga el Pronda
+try:
+    Prondamin24 = deta.Base('Prondamin2024C')
+    Pronda24 = Prondamin24.fetch(limit=5000).items
+    #dfPronda24 = pd.DataFrame(Pronda24.items)
+except:
+    'Problemas cargando Pronda'
+Pronda24
+dfPronda24 = pd.DataFrame(Pronda24)
+dfPronda24
+
+#df = pd.DataFrame.from_dict(Pronda24)
+#df
+#archicvs = 'CTAPRONDAMIN1-1.csv'
+#dfu = pd.read_csv('C:/Users/user/Downloads/CTAPRONDAMIN1-1.csv')
