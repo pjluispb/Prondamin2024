33be8ec02e076a877ed0abdbeee28197c07a75c0
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-17 16:59:18 -0400

    Update cambiakeys.py

--- pages/cambiakeys.py
+++ pages/cambiakeys.py
@@ -10,17 +10,16 @@ deta = Deta(st.secrets["deta_key"])
 
 uploaded_file = st.file_uploader("Subir archivo CSV", type=["csv"]) 
 dfu = pd.read_csv(uploaded_file)
-
+dfu
 # Carga el Pronda
 try:
     Prondamin24 = deta.Base('Prondamin2024C')
     Pronda24 = Prondamin24.fetch(limit=5000)
     dfPronda24 = pd.DataFrame(Pronda24.items)
-    st.dataframe(dfPronda24)
+    #st.dataframe(dfPronda24)
 except:
     'Problemas cargando Pronda'
-#Pronda24
-#dfPronda24 = pd.DataFrame(Pronda24)
+
 '---'
 dfPronda24
 
