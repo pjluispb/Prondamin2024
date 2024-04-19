4300f70a8e5c0aa3351455effdab8d0d8a7b7e11
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-19 15:09:44 -0400

    Update zverdata01.py

--- pages/zverdata01.py
+++ pages/zverdata01.py
@@ -2,8 +2,30 @@ import streamlit as st
 import pandas as pd
 from deta import Deta
 
-
 deta = Deta(st.secrets["deta_key"])
+
+def row_style(row):
+    if row['paycon'] == 'SI++':
+        return pd.Series('background-color: #7986cb; color:#000000', row.index)
+    elif row['paycon'] == 'PENDIENTE X DIFERENCIA':
+        return pd.Series('background-color: #ff6f00; color:#000000', row.index)
+    elif row['paycon'] == 'SI':
+        return pd.Series('background-color: #8ede99; color:#000000', row.index)
+    elif row['paycon'] == 'PENDIENTE':
+        return pd.Series('background-color: #fdd834; color:#000000', row.index)
+    else:
+        return pd.Series('', row.index)
+
+def row_style_2(row):
+    if row['Distrito'] in ('Andino', 'Centro Llanos', 'Lara', 'Llanos Occidentales', 'Nor Oriente', 'Yaracuy' ):
+        return pd.Series('background-color: #8eddf9; color:#000000', row.index)
+    else:
+        return pd.Series('background-color: #eeeeee; color:#000229', row.index)
+
+def row_style_3(row):
+    if row['index']=='Ministro Licenciado' :
+        return pd.Series('background-color: #eeeeee; color:#000229', row.index)
+
 
 @st.cache_data
 def load_data():
@@ -36,11 +58,5 @@ def load_data02():
 
 df2 = load_data02()
 df2
-
-
-
-#dfPronda24 = pd.DataFrame(Pronda24)
-
-# Simulación de un DataFrame con 4000 filas
-#data = pd.DataFrame({"key": range(4000), "nombre": range(4000), "apellido": range(4000), "categoría": range(4000), "distrito": range(4000), "paycon": range(4000)})
-#data = pd.DataFrame(Pronda24)
+df2 = df2.reindex(columns=['distrito', 'categoría', 'key', 'nombre', 'apellido', 'emails', 'teléfonos', 'modalidad', 'paycon', 'montoApagar', 'fuenteOrigen', 'referenciaPago', 'fechaPago', 'montoPago' ]) #Reordena las columnas como se mostraran
+df2.style.apply(row_style, axis=1)  #Coloriza las filas
