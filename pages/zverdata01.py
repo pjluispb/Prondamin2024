50288c42eb321824a6ca5006329ff39f8f2c2553
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-19 15:22:16 -0400

    Update zverdata01.py

--- pages/zverdata01.py
+++ pages/zverdata01.py
@@ -48,7 +48,7 @@ def load_data02():
         all_items += res.items
     dfall_items = pd.DataFrame(all_items, columns=['distrito', 'categoría', 'key', 'nombre', 'apellido', 'emails', 'teléfonos', 'modalidad', 'paycon', 'montoApagar', 'fuenteOrigen', 'referenciaPago', 'fechaPago', 'montoPago' ])
     #dfall_items_color = dfall_items.style.apply(row_style, axis=1)
-    return pd.DataFrame(dfall_items_color)
+    return pd.DataFrame(dfall_items)
     
 #df, lastdf, countdf = load_data()
 #df,  lastdf,  countdf
