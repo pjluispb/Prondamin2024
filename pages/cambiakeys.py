cd555f3c720839d1e24ea2920adc74799dc0672d
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-19 06:48:16 -0400

    Update cambiakeys.py

--- pages/cambiakeys.py
+++ pages/cambiakeys.py
@@ -16,14 +16,16 @@ def replace_value(value):
         return [str(value)]
 
 uploaded_file = st.file_uploader("Subir archivo CSV", type=["csv"]) 
-dfu = pd.read_csv(uploaded_file)
-dfu
-dfu['CédulaOLD'] = dfu['CédulaOLD'].astype(str)
-dfu['CédulaNEW'] = dfu['CédulaNEW'].astype(str)
-dfu['Teléfono'] = dfu['Teléfono'].apply(replace_value)
-dfu['Correo'] = dfu['Correo'].apply(replace_value)
-'dfu = ', dfu
-cedulas = dfu["CédulaOLD"]  
+if uploaded_file is not None:
+    # Lee el archivo CSV en un DataFrame
+    dfu = pd.read_csv(uploaded_file) 
+    dfu
+    dfu['CédulaOLD'] = dfu['CédulaOLD'].astype(str)
+    dfu['CédulaNEW'] = dfu['CédulaNEW'].astype(str)
+    dfu['Teléfono'] = dfu['Teléfono'].apply(replace_value)
+    dfu['Correo'] = dfu['Correo'].apply(replace_value)
+    'dfu = ', dfu
+    cedulas = dfu["CédulaOLD"]  
 
 # # Carga el Pronda
 Prondamin24 = deta.Base('Prondamin2024C')
