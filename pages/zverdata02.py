230a6650d62c12bec070afc92051d0e10e54b769
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-20 07:48:21 -0400

    Update zverdata02.py

--- pages/zverdata02.py
+++ pages/zverdata02.py
@@ -12,7 +12,6 @@ st.set_page_config(
 
 deta = Deta(st.secrets["deta_key"])
 
-
 def row_style(row):
     if row['paycon'] == 'SI++':
         return pd.Series('background-color: #7986cb; color:#000000', row.index)
@@ -24,5 +23,81 @@ def row_style(row):
         return pd.Series('background-color: #fdd834; color:#000000', row.index)
     else:
         return pd.Series('', row.index)
+
+@st.cache_data
+def load_dtto(DTTO):
+    Prondadtto = deta.Base('Prondamin2024C')
+    Pdtto = Prondadtto.fetch({'distrito':DTTO})
+    Pdttoitems = Pdtto.items
+    dfdtto = pd.DataFrame(Pdttoitems, columns=['distrito', 'categoría', 'key', 'nombre', 'apellido', 'emails', 'teléfonos', 'modalidad', 'paycon', 'montoApagar', 'fuenteOrigen', 'referenciaPago', 'fechaPago', 'montoPago' ])
+    #dfall_items_color = dfall_items.style.apply(row_style, axis=1)
+    return dfdtto
+
+@st.cache_data
+def load_dttoZulia():
+    ProndaZulia = deta.Base('Prondamin2024C')
+    PZulia = ProndaZulia.fetch({'distrito':'Zulia'})
+    PZuliaitems = PZulia.items
+    dfZulia = pd.DataFrame(PZuliaitems, columns=['distrito', 'categoría', 'key', 'nombre', 'apellido', 'emails', 'teléfonos', 'modalidad', 'paycon', 'montoApagar', 'fuenteOrigen', 'referenciaPago', 'fechaPago', 'montoPago' ])
+    #dfall_items_color = dfall_items.style.apply(row_style, axis=1)
+    return dfZulia
+    
+    
+@st.cache_data
+def load_data02():
+    Pronda24 = deta.Base('Prondamin2024C')
+    res = Pronda24.fetch()
+    all_items = res.items
+
+    while res.last:
+        res = Pronda24.fetch(last=res.last)
+        all_items += res.items
+    dfall_items = pd.DataFrame(all_items, columns=['distrito', 'categoría', 'key', 'nombre', 'apellido', 'emails', 'teléfonos', 'modalidad', 'paycon', 'montoApagar', 'fuenteOrigen', 'referenciaPago', 'fechaPago', 'montoPago' ])
+    #dfall_items_color = dfall_items.style.apply(row_style, axis=1)
+    return dfall_items
+
+imagen1 = Image.open('minecLogo.jpeg')
+imagen2 = Image.open('minecLogoTitle.jpeg')
+
+try:
+    logina = st.session_state['logina']
+except:
+    logina = {'user':'Picapiedra', 'Distrito':'Andino'}
+    #st.switch_page('home2024.py') 
+
+st.image(imagen1)
+st.image(imagen2)
+
+st.subheader('Hola ****' + logina['user'] + '****')
+st.write('Datos del registro de ministros del distrito: ****' + logina['Distrito'] + '****')
+
+dfdtto = load_dtto(logina['Distrito'])
+dfdtto_color = dfdtto.style.apply(row_style, axis=1)
+dfdtto_color
+
+#tab1 tab7 = st.tabs(["Por Distrito", "Todo_color"])
+#with tab1:
+#    st.subheader('Todos los distritos')
+#    st.divider()
+#    df2 = load_data02()
+#    df2
+
+#with tab7:
+#    st.subheader('Por Distrito')
+#    st.divider()
+#    with st.expander("Yaracuy"):
+#        df12 = load_dttoYaracuy()
+#        df12_color = df12.style.apply(row_style, axis=1)
+#        df12_color
+#    with st.expander("Zulia"):
+#        df13 = load_dttoZulia()
+#        df13_color = df13.style.apply(row_style, axis=1)
+#        df13_color
+
+
+
+
+st.page_link("home2024.py", label="Inicio", icon="🏠")
+
 
   
