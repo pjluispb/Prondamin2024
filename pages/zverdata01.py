e1f2cf54f9ccf35728fbe1b1ffeb9f4970983e75
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-20 16:54:55 -0400

    Update zverdata01.py

--- pages/zverdata01.py
+++ pages/zverdata01.py
@@ -222,7 +222,7 @@ with tab1:
     dfpendiente = df2[df2['paycon']=='PENDIENTE']
     dfpendiente_color = dfpendiente.style.apply(row_style, axis=1)
     dfpendiente_color
-    dfpendienteCounts = dfpendiente['paycon].value_counts()
+    dfpendienteCounts = dfpendiente['paycon'].value_counts()
     dfpendienteCounts
 # df2 = df2.reindex(columns=['distrito', 'categoría', 'key', 'nombre', 'apellido', 'emails', 'teléfonos', 'modalidad', 'paycon', 'montoApagar', 'fuenteOrigen', 'referenciaPago', 'fechaPago', 'montoPago' ]) #Reordena las columnas como se mostraran
 
