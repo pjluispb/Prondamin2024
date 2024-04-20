0a2b953bdd745836a2547c561397fdc1d70072ad
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-20 15:25:28 -0400

    Update zverdata01.py

--- pages/zverdata01.py
+++ pages/zverdata01.py
@@ -185,7 +185,7 @@ def load_dttoZulia():
 @st.cache_data
 def load_data02():
     Pronda24 = deta.Base('Prondamin2024C')
-    res = Pronda24.fetch()
+    res = Pronda24.fetch(limit=500)
     all_items = res.items
 
     while res.last:
