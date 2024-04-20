34337a016d3a958faf69d08dd3582fde3444848b
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-20 07:35:39 -0400

    Create zverdata02.py

---
+++ pages/zverdata02.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+from deta import Deta
+from PIL import Image
+
+st.set_page_config(
+    page_title="Minec Reg App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+deta = Deta(st.secrets["deta_key"])
+
+
+def row_style(row):
+    if row['paycon'] == 'SI++':
+        return pd.Series('background-color: #7986cb; color:#000000', row.index)
+    elif row['paycon'] == 'PENDIENTE X DIFERENCIA':
+        return pd.Series('background-color: #ff6f00; color:#000000', row.index)
+    elif row['paycon'] == 'SI':
+        return pd.Series('background-color: #8ede99; color:#000000', row.index)
+    elif row['paycon'] == 'PENDIENTE':
+        return pd.Series('background-color: #fdd834; color:#000000', row.index)
+    else:
+        return pd.Series('', row.index)
+
+  
