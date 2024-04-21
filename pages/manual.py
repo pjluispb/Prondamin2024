8b601b64ed15b2d5f951bcd060dc5de964173baa
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-21 17:20:39 -0400

    Update manual.py

--- pages/manual.py
+++ pages/manual.py
@@ -5,5 +5,29 @@ from deta import Deta
 deta = Deta(st.secrets["deta_key"])
 Pronda = deta.Base('Prondamin2024C')
 
-Pronda.delete(" 10126173")
+# Pronda.delete(" 10126173")
+
+reg = {
+	"key": "15733073",
+	"ReporteCertif": "-",
+	"Status": "-",
+	"apellido": "Rengifo",
+	"categoría": "Ministro Licenciado",
+	"distrito": "Nor Oriente",
+	"emails": [
+		"erengifom@yahoo.com"
+	],
+	"fechaPago": "-",
+	"fuenteOrigen": "-",
+	"modalidad": "-",
+	"montoApagar": "-",
+	"montoPago": "-",
+	"nombre": "Eduardo",
+	"paycon": "NO",
+	"referenciaPago": "-",
+	"teléfonos": [
+		"04128643490"
+	]
+}
+Pronda.put(reg)
 
