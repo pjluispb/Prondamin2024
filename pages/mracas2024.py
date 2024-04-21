0d286b4d8e83c423b2280982ed52c5187255ed2e
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-21 14:25:18 -0400

    Update mracas2024.py

--- pages/mracas2024.py
+++ pages/mracas2024.py
@@ -11,3 +11,46 @@ st.set_page_config(
 )
 
 deta = Deta(st.secrets["deta_key"])
+@st.cache_data
+def load_data02():
+    Pronda24 = deta.Base('Prondamin2024C')
+    res = Pronda24.fetch(limit=500)
+    all_items = res.items
+    while res.last:
+        res = Pronda24.fetch(last=res.last)
+        all_items += res.items
+    dfall_items = pd.DataFrame(all_items, columns=['distrito', 'categoría', 'key', 'nombre', 'apellido', 'emails', 'teléfonos', 'modalidad', 'paycon', 'montoApagar', 'fuenteOrigen', 'referenciaPago', 'fechaPago', 'montoPago' ])
+    return dfall_items
+    
+#df, lastdf, countdf = load_data()
+#df,  lastdf,  countdf
+imagen1 = Image.open('minecLogo.jpeg')
+imagen2 = Image.open('minecLogoTitle.jpeg')
+
+#try:
+#    logina = st.session_state['logina']
+#except:
+    # switch_page('reiniciar03')
+#    st.switch_page('home2024.py') 
+
+#logina = st.session_state['logina']
+#logina
+st.image(imagen1)
+st.image(imagen2)
+
+logina = {'user':'Jota', 'rol':'Admin'}
+
+st.subheader('Hola ****' + logina['user'] + '****')
+#st.write('Datos del registro de ministros del distrito: ****' + logina['Distrito'] + '****')
+
+# Carga Pronda
+Pronda = load_data02()
+
+# Carga Marks
+bdmarks = deta.Base('marks24')
+marcas = marks24.fetch(limit=5000)
+marcas_items = marcas.items
+dfmarcas = pd.DataFrame(marcas_items)
+
+'Pronda = ', Pronda
+'marks = ', marks
