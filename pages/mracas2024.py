597bbb3a0c3560d089fa2f54b96cd882382c14ca
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-21 14:47:13 -0400

    Update mracas2024.py

--- pages/mracas2024.py
+++ pages/mracas2024.py
@@ -59,8 +59,8 @@ genm = st.button('Genera Marca 1')
 if genm:
     dfcedpay = Pronda[['key','paycon']]
     dfcedpay['corte-1'] = 'Corte01 : '+dfcedpay['paycon']+' --> 21/4:3pm'
-    if dfcedpay['paycon'] == 'SI': dfcedpaycon['condicion']='Bloqueo - 01'
-    else: dfcedpaycon['condicion'] = '-'
+    if dfcedpay['paycon'] == 'SI': dfcedpay['condicion']='Bloqueo - 01'
+    else: dfcedpay['condicion'] = '-'
     'dfcedpay = ', dfcedpay
     
     
