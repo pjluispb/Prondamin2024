b73c0d27f3c5ce131bc5f96698d6348732837d4c
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-22 03:44:11 -0400

    Update manual.py

--- pages/manual.py
+++ pages/manual.py
@@ -5,21 +5,25 @@ from deta import Deta
 deta = Deta(st.secrets["deta_key"])
 Pronda = deta.Base('Prondamin2024C')
 
+exone = Pronda.fetch({'value':'exonerado'})
+dfexone = pd.DataFrame(exone.items)
+dfexone
 # Pronda.delete(" 10126173")
 
 # reg = {.....}
 
 #Pronda.put(reg)
-mask24 = deta.Base('marks24B')
-mask24f = mask24.fetch(limit=5000)
-dfmask24 = pd.DataFrame(mask24f.items)
+#--------------------------------------------------
+#mask24 = deta.Base('marks24B')
+#mask24f = mask24.fetch(limit=5000)
+#dfmask24 = pd.DataFrame(mask24f.items)
 
-regmask = dfmask24.to_dict('records')
+#regmask = dfmask24.to_dict('records')
 
-cont=1
-for registro in regmask:
-	mask24.delete(registro['key'])
-	cont+=1
-	'registro borrado ',str(cont)
-	
+#cont=1
+#for registro in regmask:
+#	mask24.delete(registro['key'])
+#	cont+=1
+#	'registro borrado ',str(cont)
+#-------------------------------------------------	
 
