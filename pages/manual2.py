fb3a54a8ffe7197c2180ed3d7604de8d27e3af7a
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-22 17:34:02 -0400

    Update manual2.py

--- pages/manual2.py
+++ pages/manual2.py
@@ -30,6 +30,8 @@ Pronda
 selected_columns = ['key', 'paycon', 'condicion', 'corte-1', 'corte-2', 'corte-3', 'distrito']  # List of desired column names
 df_selected = Pronda[selected_columns]
 df_selected
+df_selected['condicion'] = df_selected.apply(update_condicion, axis=1)
+df_selected
 #--------------------------------------------------
 # para eliminar(1ro) y luego crear(put) registros en Pronda
 #Pronda = deta.Base('Prondamin2024C')
