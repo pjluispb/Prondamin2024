5ce97f9b4770163cefdb0ed376fb0632fbc11582
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-22 15:28:58 -0400

    Update manual2.py

--- pages/manual2.py
+++ pages/manual2.py
@@ -3,3 +3,11 @@ import streamlit as st
 from deta import Deta
 
 deta = Deta(st.secrets["deta_key"])
+
+#--------------------------------------------------
+# para eliminar(1ro) y luego crear(put) registros en Pronda
+Pronda = deta.Base('Prondamin2024C')
+Pronda.delete("92ttxhdnjfnt")
+# reg = {.....}
+#Pronda.put(reg)
+#--------------------------------------------------
