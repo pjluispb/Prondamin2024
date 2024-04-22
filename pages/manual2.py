9b583940aa4497a2a174b6bd7d82fab4823edb3c
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-22 16:00:14 -0400

    Update manual2.py

--- pages/manual2.py
+++ pages/manual2.py
@@ -4,6 +4,19 @@ from deta import Deta
 
 deta = Deta(st.secrets["deta_key"])
 
+@st.cache_data
+def load_data02():
+    Pronda24 = deta.Base('Prondamin2024C')
+    res = Pronda24.fetch(limit=500)
+    all_items = res.items
+    while res.last:
+        res = Pronda24.fetch(last=res.last)
+        all_items += res.items
+    dfall_items = pd.DataFrame(all_items)
+    return dfall_items
+
+Pronda = load_data02()
+Pronda
 #--------------------------------------------------
 # para eliminar(1ro) y luego crear(put) registros en Pronda
 #Pronda = deta.Base('Prondamin2024C')
