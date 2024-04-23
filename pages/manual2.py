fbd0b4fc199e704369fd03ec417f1479243e61da
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-23 14:29:03 -0400

    Update manual2.py

--- pages/manual2.py
+++ pages/manual2.py
@@ -48,7 +48,7 @@ df_filtrado1 = Pronda[Pronda['condicion'] =='Bloqueo en marca 01']
 df_filtrado2 = Pronda[Pronda['condicion'] =='Bloqueo en marca 2']
 df_filtrado1
 df_filtrado2
-df_filtrado = pd.concat([df_filtrado1, df_filtrado2], ignore_index=True)
+df_filtrado = pd.concat([df_filtrado1, df_filtrado2])
 selected_columns = ['key', 'paycon', 'distrito', 'categoría', 'nombre', 'apellido', 'emails', 'teléfonos', 'modalidad', 'referenciaPago']         # List of desired column names
 df_selected = df_filtrado[selected_columns]
 #df_selected
