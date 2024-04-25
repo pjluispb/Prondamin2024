4511751d4b04335ea90aa771106cba1c4d3887d5
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-25 13:26:58 -0400

    Update manual3.py

--- pages/manual3.py
+++ pages/manual3.py
@@ -4,7 +4,16 @@ from deta import Deta
 from PIL import Image
 import time
 
-
+@st.cache_data
+def load_data02():
+    Pronda24 = deta.Base('Prondamin2024C')
+    res = Pronda24.fetch(limit=500)
+    all_items = res.items
+    while res.last:
+        res = Pronda24.fetch(last=res.last)
+        all_items += res.items
+    dfall_items = pd.DataFrame(all_items)
+    return dfall_items
 
 dfsis = dfPronda[dfPronda['paycon'] == 'SI']
 dfsim = dfPronda[dfPronda['paycon'] == 'SI++']
