7308a67002184930f585f4b99565bcad1c6327e5
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-27 12:14:51 -0400

    Update manual3.py

--- pages/manual3.py
+++ pages/manual3.py
@@ -38,8 +38,8 @@ if uploaded_file is not None:
 #dfpyd = pd.merge(dfPronda24_ref, DatBanVerif, on='referenciaPago', how='left')   # Mezcla Pronda y DatBanVerif   
 #dfpymarks = pd.merge(dfpronda2, dfmarks, on='key', how='left')
 #result = pd.merge(df, dfLaraSC, on='emails', how='left')
-result = pd.merge(dfLaraSC, df, on='emails', how='left')
-#result = pd.merge(dfLaraSC, df, left_on=['nombre', 'apellido'], right_on=['nombre', 'apellido'])
+#result = pd.merge(dfLaraSC, df, on='emails', how='left')
+result = pd.merge(dfLaraSC, df, left_on=['emails'], right_on=['emails'])
 'result = ', result
 st.write(result['test01'].value_counts())
 
