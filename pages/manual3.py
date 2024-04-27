b346b793d81e8dd85268815fa5080f0b6e3eb80b
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-27 12:09:29 -0400

    Update manual3.py

--- pages/manual3.py
+++ pages/manual3.py
@@ -37,7 +37,7 @@ if uploaded_file is not None:
 '---'    
 #dfpyd = pd.merge(dfPronda24_ref, DatBanVerif, on='referenciaPago', how='left')   # Mezcla Pronda y DatBanVerif   
 #dfpymarks = pd.merge(dfpronda2, dfmarks, on='key', how='left')
-result = pd.merge(dfLaraSC, df, on='emails', how='left')
+result = pd.merge(df, dfLaraSC, on='emails', how='left')
 #result = pd.merge(dfLaraSC, df, on=
 #result = pd.merge(dfLaraSC, df, left_on=['nombre', 'apellido'], right_on=['nombre', 'apellido'])
 'result = ', result
