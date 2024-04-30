6a027cf5e078b5db87262366f0f40da4b083c3f0
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-30 08:05:40 -0400

    Update manual4.py

--- pages/manual4.py
+++ pages/manual4.py
@@ -16,7 +16,19 @@ def load_data02():
     dfall_items = pd.DataFrame(all_items)
     return dfall_items
 
-
+def update_condicion(row):
+    #'row : ', row
+    #fmontoPago = float(row['montoPago']) if row['montoPago'] not in ('-', None, '') else 0
+    if row['paycon'] in ['NO', 'PENDIENTE', 'PENDIENTE X DIFERENCIA']:
+        return '-'
+    else:
+        if row['corte-1'] != '-':
+            return 'Bloqueo en marca 01'
+        else:
+            if row['corte-2'].find('SI')>0:
+                return 'Bloqueo en marca 2'
+            else:
+                return 'Bloqueo en marca 3'
 
 Pronda = load_data02()
 
@@ -24,7 +36,7 @@ sel_col = ['key',  'distrito',  'nombre', 'apellido', 'teléfonos']         # Li
 dfProndaSC = Pronda[sel_col]
 dfProndaSC['teléfonos'] = dfProndaSC['teléfonos'].apply(lambda x: str(x[0]) if x else '')
 dfProndaSC['telefFormat'] = '-'
-dfProndaSC['telefFormat'] = dfProndaSC.apply(formatelf, axis=1)
+#dfProndaSC['telefFormat'] = dfProndaSC.apply(formatelf, axis=1)
 'pronda = ', dfProndaSC
 st.write(dfProndaSC['teléfonos'].value_counts())
 
