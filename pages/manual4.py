1edaa55b38b15fb660e501fa0195496ef5e1931d
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-30 08:14:35 -0400

    Update manual4.py

--- pages/manual4.py
+++ pages/manual4.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 from deta import Deta
 import time
+import re
 
 deta = Deta(st.secrets["deta_key"])
 
@@ -16,19 +17,16 @@ def load_data02():
     dfall_items = pd.DataFrame(all_items)
     return dfall_items
 
-def update_condicion(row):
+def formatelf(row):
     #'row : ', row
     #fmontoPago = float(row['montoPago']) if row['montoPago'] not in ('-', None, '') else 0
-    if row['paycon'] in ['NO', 'PENDIENTE', 'PENDIENTE X DIFERENCIA']:
-        return '-'
-    else:
-        if row['corte-1'] != '-':
-            return 'Bloqueo en marca 01'
-        else:
-            if row['corte-2'].find('SI')>0:
-                return 'Bloqueo en marca 2'
-            else:
-                return 'Bloqueo en marca 3'
+    digitos = re.sub(r'\D', '', str(row['teléfonos']))
+    return digitos
+    #if row['teléfonos'].startswith('-0'):
+    #    newt = row['teléfonos'][1:]
+    #    return newt
+    #elif row['teléfonos'].
+    
 
 Pronda = load_data02()
 
@@ -36,7 +34,7 @@ sel_col = ['key',  'distrito',  'nombre', 'apellido', 'teléfonos']         # Li
 dfProndaSC = Pronda[sel_col]
 dfProndaSC['teléfonos'] = dfProndaSC['teléfonos'].apply(lambda x: str(x[0]) if x else '')
 dfProndaSC['telefFormat'] = '-'
-#dfProndaSC['telefFormat'] = dfProndaSC.apply(formatelf, axis=1)
+dfProndaSC['telefFormat'] = dfProndaSC.apply(formatelf, axis=1)
 'pronda = ', dfProndaSC
 st.write(dfProndaSC['teléfonos'].value_counts())
 
