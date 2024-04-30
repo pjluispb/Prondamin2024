e38de9f179457ea8296697f8f8981b81da0f903a
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-04-30 11:34:39 -0400

    Update manual4.py

--- pages/manual4.py
+++ pages/manual4.py
@@ -18,8 +18,6 @@ def load_data02():
     return dfall_items
 
 def formatelf(row):
-    #'row : ', row
-    #fmontoPago = float(row['montoPago']) if row['montoPago'] not in ('-', None, '') else 0
     digitos = re.sub(r'\D', '', str(row['teléfonos']))
     newt = digitos
     if digitos=='': newt = '-'
@@ -60,9 +58,9 @@ Pronda = load_data02()
 
 sel_col = ['key',  'distrito',  'nombre', 'apellido', 'teléfonos']         # List of desired column names
 dfProndaSC = Pronda[sel_col]
-dfProndaSC['teléfonos'] = dfProndaSC['teléfonos'].apply(lambda x: str(x[0]) if x else '')
-dfProndaSC['telefFormat'] = '-'
-dfProndaSC['telefFormat'] = dfProndaSC.apply(formatelf, axis=1)
+#dfProndaSC['teléfonos'] = dfProndaSC['teléfonos'].apply(lambda x: str(x[0]) if x else '')
+#dfProndaSC['telefFormat'] = '-'
+dfProndaSC['teléfonos'] = dfProndaSC.apply(formatelf, axis=1)
 'pronda = ', dfProndaSC
-st.write(dfProndaSC['telefFormat'].value_counts())
+st.write(dfProndaSC['teléfonos'].value_counts())
 
