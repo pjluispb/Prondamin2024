1108a1087434cfaa3b0599da045e91b4b65b3d06
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-05-02 14:49:47 -0400

    Update califica.py

--- pages/califica.py
+++ pages/califica.py
@@ -17,18 +17,21 @@ dfcMLi = dfcML.reindex(columns=['CATEGORIA','DISTRITO','NOMBRES','APELLIDOS','CU
 dfcMOi = dfcMO.reindex(columns=['CATEGORIA','DISTRITO','NOMBRES','APELLIDOS','CURSOREALIZADO','Total del curso (Real)','STATUS','MODALIDAD','CEDULA','EMAIL','TELEFONO','REPORTECERTIF','key'])
 
 'Ministros Cristianos Bloque 01 : ', dfcMCi
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 col1.write(dfcMCi['DISTRITO'].value_counts())
-col2.write(dfcMCi['Total del curso (Real)'].value_counts())
+col2.write(dfcMCi['STATUS'].value_counts())
+col3.write(dfcMCi['Total del curso (Real)'].value_counts())
 '---'
 'Ministros Licenciados Bloque 01 : ', dfcMLi
-col3, col4 = st.columns(2)
-col3.write(dfcMLi['DISTRITO'].value_counts())
-col4.write(dfcMLi['Total del curso (Real)'].value_counts())
+col4, col5, col6 = st.columns(3)
+col4.write(dfcMLi['DISTRITO'].value_counts())
+col5.write(dfcMLi['STATUS'].value_counts())
+col6.write(dfcMLi['Total del curso (Real)'].value_counts())
 '---'
 'Ministros Ordenados Bloque 01 : ', dfcMOi
-col3, col4 = st.columns(2)
-col3.write(dfcMOi['DISTRITO'].value_counts())
-col4.write(dfcMOi['Total del curso (Real)'].value_counts())
+col7, col8, col9 = st.columns(3)
+col7.write(dfcMOi['DISTRITO'].value_counts())
+col8.write(dfcMOi['STATUS'].value_counts())
+col9.write(dfcMOi['Total del curso (Real)'].value_counts())
 '---'
 
