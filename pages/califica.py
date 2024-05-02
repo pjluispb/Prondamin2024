a5744ac9eb5e49a9535c099f3d32ff20afc49fd8
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-05-02 08:22:57 -0400

    Update califica.py

--- pages/califica.py
+++ pages/califica.py
@@ -15,5 +15,13 @@ dfcMCi = dfcMC.reindex(columns=['CATEGORIA','DISTRITO','NOMBRES','APELLIDOS','CU
 dfcMLi = dfcMC.reindex(columns=['CATEGORIA','DISTRITO','NOMBRES','APELLIDOS','CURSOREALIZADO','Total del curso (Real)','STATUS','MODALIDAD','CEDULA','EMAIL','TELEFONO','REPORTECERTIF','key'])
 
 'Ministros Cristianos Bloque 01 : ', dfcMCi
+col1, col2 = st.columns(2)
+col1.write(dfcMCi['DISTRITO'].value_counts())
+col2.write(dfcMCi['Total del curso (Real)'].value_counts())
 '---'
 'Ministros Licenciados Bloque 01 : ', dfcMLi
+col3, col4 = st.columns(2)
+col3.write(dfcMLi['DISTRITO'].value_counts())
+col4.write(dfcMLi['Total del curso (Real)'].value_counts())
+'---'
+
