5e414dc1dc13bb9789ba016ee8240b51e4cf565d
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-05-02 08:13:19 -0400

    Update califica.py

--- pages/califica.py
+++ pages/califica.py
@@ -11,7 +11,9 @@ dfProndaCalifica = pd.DataFrame(itemsProndaCalifica)
 
 dfcMC = dfProndaCalifica.loc[dfProndaCalifica['CURSOREALIZADO'] == 'Ministro Cristiano ']
 dfcML = dfProndaCalifica.loc[dfProndaCalifica['CURSOREALIZADO'] == 'Ministro Licenciado ']
+dfcMCi = dfcMC.reindex(columns=['CATEGORIA','DISTRITO','NOMBRES','APELLIDOS','CURSOREALIZADO','Total del curso (Real)','STATUS','MODALIDAD','CEDULA','EMAIL','TELEFONO','REPORTECERTIF','key'])
+dfcMLi = dfcMC.reindex(columns=['CATEGORIA','DISTRITO','NOMBRES','APELLIDOS','CURSOREALIZADO','Total del curso (Real)','STATUS','MODALIDAD','CEDULA','EMAIL','TELEFONO','REPORTECERTIF','key'])
 
-'Ministros Cristianos Bloque 01 : ', dfcMC
+'Ministros Cristianos Bloque 01 : ', dfcMCi
 '---'
-'Ministros Licenciados Bloque 01 : ', dfcML
+'Ministros Licenciados Bloque 01 : ', dfcMLi
